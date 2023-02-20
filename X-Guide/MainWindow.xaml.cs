d12eb6bee643553340c5077046d94dd7b204ecd3
ongzhen119 <ongzc19@gmail.com>
2023-02-20 15:42:25 +0800

    update

--- X-Guide/MainWindow.xaml.cs
+++ X-Guide/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace X_Guide
 {
@@ -23,19 +24,22 @@ namespace X_Guide
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                this.DateTimeTextBlock.Text = DateTime.Now.ToString("dddd , MMM dd yyyy,hh:mm:ss");
+            }, this.Dispatcher);
         }
 
-        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+     
+        //Purpose: Double click Application for maximize window/Normal size 
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.ChangedButton == MouseButton.Left)
-                if (e.ClickCount == 2)
-                {
-                    AdjustWindowSize();
-                }
-                else
-                {
-                    Application.Current.MainWindow.DragMove();
-                }
+            if (e.ClickCount == 2)      //Double Click 
+                //Compare current window state, if maximize , then the window state is changed to normal or vice versa 
+                WindowState = (WindowState == WindowState.Maximized) ?
+                    WindowState.Normal : WindowState.Maximized;
+            else
+                DragMove(); //Drag the window 
         }
 
         /// <summary>
@@ -79,15 +83,38 @@ namespace X_Guide
             }
 
         }
-
         private void Expander_Expanded(object sender, RoutedEventArgs e)
         {
-            var expandedExpander = sender as Expander;
+            Expander expander = (Expander)sender;
 
-            foreach (var expander in stackPanel.Children.OfType<Expander>().Where(expander => expander != expandedExpander))
+            foreach (Expander otherExpander in FindVisualChildren<Expander>(this))
             {
-                expander.IsExpanded = false;
+                if (otherExpander != expander)
+                {
+                    otherExpander.IsExpanded = false;
+                }
             }
         }
+
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject parent) where T : DependencyObject
+        {
+            if (parent != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
     }
 }
