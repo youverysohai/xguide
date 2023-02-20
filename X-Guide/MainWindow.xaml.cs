ac290d4c78c304293ead1809ba38a7018d78eb81
ongzhen119 <ongzc19@gmail.com>
2023-02-20 11:56:45 +0800

    changed ui

--- X-Guide/MainWindow.xaml.cs
+++ X-Guide/MainWindow.xaml.cs
@@ -24,5 +24,70 @@ namespace X_Guide
         {
             InitializeComponent();
         }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                if (e.ClickCount == 2)
+                {
+                    AdjustWindowSize();
+                }
+                else
+                {
+                    Application.Current.MainWindow.DragMove();
+                }
+        }
+
+        /// <summary>
+        /// CloseButton_Clicked
+        /// </summary>
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// MaximizedButton_Clicked
+        /// </summary>
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            AdjustWindowSize();
+        }
+
+        /// <summary>
+        /// Minimized Button_Clicked
+        /// </summary>
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// Adjusts the WindowSize to correct parameters when Maximize button is clicked
+        /// </summary>
+        private void AdjustWindowSize()
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+                //MaxButton.Content = "1";
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+                //MaxButton.Content = "2";
+            }
+
+        }
+
+        private void Expander_Expanded(object sender, RoutedEventArgs e)
+        {
+            var expandedExpander = sender as Expander;
+
+            foreach (var expander in stackPanel.Children.OfType<Expander>().Where(expander => expander != expandedExpander))
+            {
+                expander.IsExpanded = false;
+            }
+        }
     }
 }
