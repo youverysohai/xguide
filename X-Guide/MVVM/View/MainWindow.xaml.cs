a57caaf502967ee0a68e25b7976b77d11e6f68a7
ongzhen119 <ongzc-pm19@student.tarc.edu.my>
2023-03-20 09:52:24 +0800

    main window Side navigation menu

--- X-Guide/MVVM/View/MainWindow.xaml.cs
+++ X-Guide/MVVM/View/MainWindow.xaml.cs
@@ -29,138 +29,23 @@ namespace X_Guide
             InitializeComponent();
             DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
             {
-                this.DateTimeTextBlock.Text = DateTime.Now.ToString("dddd, MMM dd yyyy, hh:mm:ss");
+                this.SideMenuNavigation.PaneTitle = DateTime.Now.ToString("dddd, MMM dd yyyy, hh:mm:ss");
             }, this.Dispatcher);
         }
 
 
-        //Purpose: Double click Application for maximize window/Normal size 
-        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ClickCount == 2)      //Double Click 
-                //Compare current window state, if maximize , then the window state is changed to normal or vice versa 
-                WindowState = (WindowState == WindowState.Maximized) ?
-                    WindowState.Normal : WindowState.Maximized;
-            else
-                DragMove(); //Drag the window 
-        }
-
-        /// <summary>
-        /// CloseButton_Clicked
-        /// </summary>
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            System.Diagnostics.Process.GetCurrentProcess().Kill();
-            Application.Current.Shutdown();
-        }
-
-        /// <summary>
-        /// MaximizedButton_Clicked
-        /// </summary>
-        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            AdjustWindowSize();
-        }
-
-        /// <summary>
-        /// Minimized Button_Clicked
-        /// </summary>
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
-
-        /// <summary>
-        /// Adjusts the WindowSize to correct parameters when Maximize button is clicked
-        /// </summary>
-        private void AdjustWindowSize()
-        {
-            if (this.WindowState == WindowState.Maximized)
-            {
-                this.WindowState = WindowState.Normal;
-                //MaxButton.Content = "1";
-            }
-            else
-            {
-                this.WindowState = WindowState.Maximized;
-                //MaxButton.Content = "2";
-            }
-
-        }
-        private void Expander_Expanded(object sender, RoutedEventArgs e)
-        {
-            Expander expander = (Expander)sender;
-
-            foreach (Expander otherExpander in FindVisualChildren<Expander>(this))
-            {
-                if (otherExpander != expander)
-                {
-                    otherExpander.IsExpanded = false;
-                }
-            }
-        }
-
-        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject parent) where T : DependencyObject
-        {
-            if (parent != null)
-            {
-                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
-                {
-                    DependencyObject child = VisualTreeHelper.GetChild(parent, i);
-                    if (child != null && child is T)
-                    {
-                        yield return (T)child;
-                    }
-
-                    foreach (T childOfChild in FindVisualChildren<T>(child))
-                    {
-                        yield return childOfChild;
-                    }
-                }
-            }
-        }
-
-        private void HamburgerMenuButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Get a reference to the application resources
-            ResourceDictionary resources = Application.Current.Resources;
-            Color foregroundColor = (Color)resources["PrimaryTextColor"]; //white
 
 
-            // Get the SolidColorBrush resource from the resource dictionary
-            SolidColorBrush foregroundBrush = (SolidColorBrush)resources["PrimaryBlueColor"];
-            SolidColorBrush foregroundWhiteBrush = new SolidColorBrush(foregroundColor);
-            if (SideMenu.Width > 50)
-            {
-                TextElement.SetForeground(HambergerMenuBtn, foregroundWhiteBrush);
-                SideMenu.Width = 50;
-                TextTitle.Visibility = Visibility.Collapsed;
-                Username.Visibility = Visibility.Collapsed;
-                TxtRoleIcon.Visibility = Visibility.Collapsed;
-                UserControlContent.SetValue(Grid.ColumnProperty, 2);
-                UserControlContent.SetValue(Grid.ColumnSpanProperty, 3);
+       
 
-            }
-            else
-            {
-                TextElement.SetForeground(HambergerMenuBtn, foregroundBrush);
-                SideMenu.Width = 180;
-                TextTitle.Visibility = Visibility.Visible;
-                Username.Visibility = Visibility.Visible;
-                TxtRoleIcon.Visibility = Visibility.Visible;
-                UserControlContent.SetValue(Grid.ColumnProperty, 3);
-                UserControlContent.SetValue(Grid.ColumnSpanProperty, 1);
 
-            }
-        }
-
-        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            if(sender != null)
-            {
-                ((MainViewModel)DataContext).InputPassword = ((PasswordBox)sender).SecurePassword;
-            }
-        }
+        //private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        //{
+        //    if(sender != null)
+        //    {
+        //        ((MainViewModel)DataContext).InputPassword = ((PasswordBox)sender).SecurePassword;
+        //    }
+        //}
     }
 
 }
