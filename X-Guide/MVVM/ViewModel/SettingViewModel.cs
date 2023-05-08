f8a569cce4ca09e8790c2f9ab35ee34d05760f3d
ongzhen119 <ongzc-pm19@student.tarc.edu.my>
2023-05-08 15:23:39 +0800

    update setting layout

--- X-Guide/MVVM/ViewModel/SettingViewModel.cs
+++ X-Guide/MVVM/ViewModel/SettingViewModel.cs
@@ -74,6 +74,7 @@ namespace X_Guide.MVVM.ViewModel
         private async void OpenVisionForm(object obj)
         {
             Vision = new VisionViewModel();
+            Vision = ((VisionViewModel)obj).Clone() as VisionViewModel;
             if (obj is ContentDialog dialog) await dialog.ShowAsync();
         }
 
