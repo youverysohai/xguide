860d6abfa82dc3b5449b76180d430fa3a15ff319
chun <ongzc19@gmail.com>
2023-07-13 17:10:25 +0800

    Layout adjustment

---
+++ X-Guide/Converter/IndexConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace X_Guide.Converter
+{
+    public class IndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is ObservableCollection<object> collection))
+                return -1;
+
+            return collection.IndexOf(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
